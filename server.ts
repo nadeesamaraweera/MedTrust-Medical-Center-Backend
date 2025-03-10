0d3443808ac67a2b5afdf0d40f6e1a3dfebd5df2
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2025-03-10 11:48:09 +0530

    created patientStore.ts with all crud functionalitie

--- server.ts
+++ server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import departmentRoutes from "./routes/departmentRoute";
+import doctorRoutes from "./routes/doctorRoute";
+import nurseRoutes from "./routes/nurseRoute";
 
-const app=express();
+
+const app = express();
 
 app.use(express.json());
 
@@ -12,6 +16,14 @@ app.use('/',(req,res,next)=>{
     next();
 })
 
+app.use('/department',departmentRoutes);
+app.use('/doctor',doctorRoutes);
+app.use('/nurse',nurseRoutes);
+app.use('/patient',nurseRoutes);
+
+app.listen(3003, (err=>{
+    console.log("Server running on port 3003");
+}));
 
 app.use('/',(req,res,next)=>{
     res.status(200).send('Not Found');
